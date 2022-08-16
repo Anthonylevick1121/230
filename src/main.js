d08ee9d80a0c51be35035b8829ea08bf081cb291
Anthony Levick <acl9861@g.rit.edu>
2022-08-15 23:08:19 -0400

    start of 330 repository

---
+++ src/main.js
@@ -0,0 +1,39 @@
+
+	"use strict";
+	
+	const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+	
+	const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+	
+	const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+    
+    generate(1);
+    //document.querySelector("#myButton").onclick=generate;
+    document.querySelector("#mybutton").addEventListener("click", function()
+    {
+        console.log("check");
+        generate(1);
+    });
+    document.querySelector("#fivebutton").addEventListener("click", function(){
+        generate(5);
+    });
+    
+
+    function randomElement(Array)
+        {
+            return Array[Math.floor(Math.random() * Array.length)];
+        }
+
+    function generate(num)
+    {
+       let str="";
+       for(let value=0; value<num; value++)
+       {
+        str = str + `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}` + "<br/>";
+       }
+        
+        document.querySelector("#output").innerHTML =str;
+    }
+
+  
